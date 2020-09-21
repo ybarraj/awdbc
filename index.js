2f7c2cbea9352f1b915dea389357e7e4feec1a4f
Joseph Ybarra <josephybarra@me.com>
2020-09-21 23:11:57 +0000

    DB setup

---
+++ index.js
@@ -0,0 +1,11 @@
+var express     = require('express'), 
+    app         = express(),
+    port        = process.env.PORT;
+    
+app.get("/", function(req,res){
+    res.send("Hi there from express");
+});
+
+app.listen(port, function(){
+    console.log("APP IS RUNNING ON " + process.env.PORT);
+});
