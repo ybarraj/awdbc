898b06563d2a73330e0ce93bf4148fa9e1599375
Joseph Ybarra <josephybarra@me.com>
2020-09-22 21:31:42 +0000

    API completed

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express     = require('express'), 
     app         = express(),
-    port        = process.env.PORT;
-    
+    port        = process.env.PORT,
+    bodyParser  = require('body-parser');
+
+var todoRoutes = require('./routes/todos');
+
+
+// I think we need to add a require statment here for the models     
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+   
+
 app.get("/", function(req,res){
-    res.send("Hi there from express");
+    res.send("HELLO FROM THE INDEX ROUTE");
 })
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
     console.log("APP IS RUNNING ON " + process.env.PORT);
+    console.log("module.path = " + module.paths)
 });
