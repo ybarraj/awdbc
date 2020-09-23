fb51a50d9dbe4be76f0b91d19f51bef69f710d33
Joseph Ybarra <josephybarra@me.com>
2020-09-23 17:35:14 +0000

    API completed

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
     res.send("HELLO FROM THE INDEX ROUTE");
-})
+});
 
 app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
     console.log("APP IS RUNNING ON " + process.env.PORT);
-    console.log("module.path = " + module.paths)
 });
