39052156fb4283a02f886ced1b70e0362511f247
Joseph Ybarra <josephybarra@me.com>
2020-09-24 17:07:43 +0000

    app finished

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-   
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
-app.get("/", function(req,res){
-    res.send("HELLO FROM THE INDEX ROUTE");
+app.get('/', function(req, res){
+    res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
